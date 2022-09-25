41fad969aab7733af36840b699909d79c0453eb7
JerrySC68 <jerrysc68@gmail.com>
2022-09-25 16:39:44 -0600

    //Clases y archvios de sucursales agregados

---
+++ Sucursales/src/main/java/proyecto1/presentation/acercade/view.java
@@ -0,0 +1,21 @@
+package proyecto1.presentation.acercade;
+
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.layout.element.Image;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class view {
+    private JPanel acerdaDePanel;
+    private JPanel imagenLbl;
+
+    /*public View(){
+        imagenLbl.createImage(700,700){
+            Image imagen = new Image(ImageDataFactory.create("logo.jpg");
+            ImageIcon iconLogo = new ImageIcon("logo.jpg");
+            imagenLbl.setIcon(iconLogo);
+            imagenLbl.setIco;
+        };
+    }*/
+}
